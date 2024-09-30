7224b050ef62b40a589d22e72984b8da72d757e7
eeseul <sseul2323@gmail.com>
2024-09-30 22:44:12 +0900

    페이지 라이터 설정

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -1,8 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "./pages/Home"
+import New from "./pages/New"
+import Diary from "./pages/Diary"
+import Notfound from "./pages/Notfound"
+
 function App() {
   return (
-    <>
-      App
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
   )
 }
 
