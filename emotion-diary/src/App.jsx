cc3c30dc9ef368372b41d42eb341d304ac5fd949
eeseul <sseul2323@gmail.com>
2024-09-30 22:52:49 +0900

    이모지 이미지 추가 및 설정

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -6,14 +6,19 @@ import New from "./pages/New"
 import Diary from "./pages/Diary"
 import Notfound from "./pages/Notfound"
 
+import { getEmotionImage } from './util/get-emotion-image';
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+      <img src={getEmotionImage(1)}/>
+    </>
   )
 }
 
