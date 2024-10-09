f038c1c5aa7c354b281c0eafd95cd85b43db0e4b
logi023 <sseul2323@gmail.com>
2024-10-09 21:08:22 +0900

    일기 삭제기능 추가

--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -1,10 +1,38 @@
-import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { DiaryDispatchContext } from '../App';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
+    const { onDelete } = useContext(DiaryDispatchContext);
+
+    const onClickDelete = () => {
+        if(window.confirm('일기를 정말 삭제할까요?')) {
+            onDelete(params.id);
+            nav('/', { replace:true })
+        }
+    }
 
     return (
-        <div>{params.id}번째 수정페이지</div>
+        <div className='New'>
+            <Header 
+                title={'일기 수정하기'} 
+                leftChild={<Button 
+                    onClick={() => nav(-1)}
+                    text={'< 뒤로 가기'}    
+                />}
+                rightChild={<Button 
+                    onClick={onClickDelete}
+                    text={'삭제하기'} 
+                    type={'NEGATIVE'} 
+                />}
+            />
+            <Editor />
+        </div>
     )
 }
 export default Edit;
