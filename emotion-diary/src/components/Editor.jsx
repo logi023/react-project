65c5bf8d20f5b515fc124751e8d6cfbe6264e52d
logi023 <sseul2323@gmail.com>
2024-10-09 20:59:45 +0900

    New 페이지 UI 및 기능구현

---
+++ emotion-diary/src/components/Editor.jsx
@@ -0,0 +1,138 @@
+import './Editor.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import EmotionItem from '../components/EmotionItem';
+import Button from '../components/Button';
+
+const emotionList = [
+    {
+        emotionId: 1,
+        emotionName: '완전 좋음',
+    },
+    {
+        emotionId: 2,
+        emotionName: '좋음',
+    },
+    {
+        emotionId: 3,
+        emotionName: '보통',
+    },
+    {
+        emotionId: 4,
+        emotionName: '나쁨',
+    },
+    {
+        emotionId: 5,
+        emotionName: '완전 나쁨',
+    },
+];
+
+const getStringedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth();
+    let date = targetDate.getDate();
+
+    if(month < 10) {
+        month = `0${month}`
+    }
+    if(date < 10) {
+        date = `0${date}`
+    }
+
+    return `${year}-${month}-${date}`;
+}
+
+const Editor = ({ onSubmit }) => {
+    const nav = useNavigate();
+    const [userInput, setUserInput] = useState({
+        createdDate: new Date(),
+        emotionId: 1,
+        content: '',
+    });
+
+    const getStringedDate = (targetDate) => {
+        let year = targetDate.getFullYear();
+        let month = targetDate.getMonth() + 1;
+        let date = targetDate.getDate();
+
+        if(month<10) {
+            month = `0${month}`
+        }
+        if(date<10) {
+            date = `0${date}`
+        }
+
+        return `${year}-${month}-${date}`;
+    }
+
+    const onChangeInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        
+        if(name === 'createdDate') {
+            value = new Date(value);
+        }
+    
+        setUserInput({
+            ...userInput,
+            [name]: value,
+        })
+    }
+
+    const onClickSubmitButton = () => {
+        onSubmit(userInput)
+    }
+
+    return (
+        <div className="Editor">
+            <section className='date_section'>
+                <h4>오늘의 날짜</h4>
+                <input 
+                    name='createdDate'
+                    onChange={onChangeInput}
+                    value={getStringedDate(userInput.createdDate)}
+                    type="date"     
+                />
+            </section>
+            <section className='emotion_section'>
+                <h4>오늘의 감정</h4>
+                <div className="emotion_list_wrapper">
+                    {emotionList.map((item) => 
+                        <EmotionItem 
+                            onClick={() => onChangeInput({
+                                target: {
+                                    name: 'emotionId',
+                                    value: item.emotionId,
+                                }
+                            })}
+                            key={item.emotionId} 
+                            {...item} 
+                            isSelected={item.emotionId === userInput.emotionId}
+                        />
+                    )}
+                </div>
+            </section>
+            <section className='content_section'>
+                <h4>오늘의 일기</h4>
+                <textarea 
+                    name='content'
+                    value={userInput.content}
+                    onChange={onChangeInput}
+                    placeholder='오늘은 어땠나요?' 
+                />
+            </section>
+            <section className='button_section'>
+                <Button 
+                    onClick={()=> nav(-1)}
+                    text={'취소하기'}     
+                />
+                <Button 
+                    onClick={onClickSubmitButton}
+                    text={'작성완료'} 
+                    type={'POSITIVE'} 
+                />
+            </section>
+        </div>
+    )
+}
+export default Editor;
--- emotion-diary/src/pages/New.jsx
+++ emotion-diary/src/pages/New.jsx
@@ -1,6 +1,37 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from '../App';
+
+import Header from "../components/Header";
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+
 const New = () => {
+    const nav = useNavigate();
+    const { onCreate } = useContext(DiaryDispatchContext);
+
+    const onSubmit = (userInput) => {
+        onCreate(
+            userInput.createdDate.getTime(),
+            userInput.emotionId,
+            userInput.content,
+        );
+        nav('/', { replace:true })
+    }
+
     return (
-        <div>New</div>
+        <div className="New">
+            <Header 
+                title={'새 일기 쓰기'}
+                leftChild={<Button 
+                    onClick={() => nav(-1)}
+                    text={'< 뒤로가기'} 
+                />}
+            />
+            <Editor 
+                onSubmit={onSubmit}
+            />
+        </div>
     )
 }
 export default New;
